090775c0ebfa0f725a934c85b9e2fbfab5cabd15
Gideonjon <gideonjones63@gmail.com>
2023-05-21 22:15:01 +0100

    idan

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import { inject } from '@vercel/analytics';
+ 
+// import some helper that is exposed by your current framework to determine the right mode manually
+import { dev } from '$app/environment';
+ 
+inject({
+  mode: dev ? 'development' : 'production',
+});
